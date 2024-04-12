33898ae1846f8aaafa831edff000f0cd404dd3fa
ajdin <ajdin.work@gmail.com>
2024-04-12 11:30:28 +0200

    finished REST section

---
+++ rest-section/04-crud-with-spring-data-rest/src/main/java/com/ishowdarkside/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.ishowdarkside.cruddemo.dao;
+
+import com.ishowdarkside.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+};
