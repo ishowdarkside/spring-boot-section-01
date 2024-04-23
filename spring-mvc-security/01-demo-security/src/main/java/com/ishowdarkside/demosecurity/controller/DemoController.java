fdcd5f02dc9cc6e73660a438123fc58063a09689
ajdin <ajdin.work@gmail.com>
2024-04-23 12:06:47 +0200

    Restricted access to unauthorized users

---
+++ spring-mvc-security/01-demo-security/src/main/java/com/ishowdarkside/demosecurity/controller/DemoController.java
@@ -0,0 +1,31 @@
+package com.ishowdarkside.demosecurity.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showForm(){
+        return "home";
+    }
+
+
+    // add a request mapping for /leaders
+
+    @GetMapping("/leaders")
+    public String showLeaders(){
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems(){
+        return "systems";
+    }
+
+
+
+}
